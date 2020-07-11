16d5688856062dfc1a80c27ca9063c019f5c24c7
Ruxue <1078134231@qq.com>
2020-07-11 19:02:41 +0800

    1.test测试,像本地仓库添加文件

---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<u-action-sheet :list="list" v-model="show"></u-action-sheet>
+	</view>
+</template>
+
+<script>
+	export default {
+	data() {
+		return {
+			title:'test',
+			list: [{
+				text: '点赞',
+				color: 'blue',
+				fontSize: 28
+			}, {
+				text: '分享'
+			}, {
+				text: '评论'
+			}],
+			show: true
+		}
+	}
+}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
