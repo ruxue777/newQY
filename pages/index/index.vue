8d6d7b2ef3a67a8907c1c3cc945dba47786a4508
Ruxue <1078134231@qq.com>
2020-07-17 21:21:03 +0800

    第一次修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,7 @@
 <template>
 	<view>
-		<!-- <u-popup v-model="show">
-			<view>出淤泥而不染，濯清涟而不妖</view>
-		</u-popup>
-		<u-button @click="show = true">打开</u-button> -->
-		<top_seek></top_seek>
+		<topSeek></topSeek>
+		
 	</view>
 </template>
 
@@ -12,69 +9,32 @@
 //请求组件
 import {request} from '../../api/request.js'
 //样式组件
-import top_seek from "./components/top_seek.vue";	
+import topSeek from "./components/topSeek.vue";	
 	
 export default {
 	components:{
-		"top_seek":top_seek
+		"topSeek":topSeek
 	},
 	data() {
 		return {
-			title:'test',
-			list: [{
-				text: '点赞',
-				color: 'blue',
-				fontSize: 28
-			}, {
-				text: '分享'
-			}, {
-				text: '评论'
-			}],
-			show: false,
-			keyword:''
+			
 		};
 	},
 	onLoad() {
 		this.getData()
 	},
 	methods:{
-		getData(){
-			let url = 'API_GetList_BusinessCircleSearch'
-			let data = {Keywords:'',
-						pageSize:8,
-						index:1}
-			request(url,data).then(res=>{
-				console.log(res)
-			})
+		getData(){	
+						
+			request('API_GetList_BusinessCircleSearch',{Keywords:'',pageSize:8,index:1})
+				.then(res=>{
+					console.log(res)
+				})
 		}
 	}
 }
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="less" scoped>
+	
 </style>
