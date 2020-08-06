6fbdcefd4e5f239a1eddd962df084fbf047b215b
Ruxue_Co <1078134231@qq.com>
2020-08-06 17:55:03 +0800

    修改新用户页面

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,167 +1,36 @@
 <template>
-	<view>
-		<view class="back-img">
-			<image class="cont-img" src="/static/image/background-image.png"></image>
-		</view>
-		
-		<view class="avatar">
-			<u-avatar :src="userInfo.WM_headimgurl" mode="circle" size="150"></u-avatar>
-			<image class="grade-img" src="../../static/image/grade.png"></image>
-			<text class="grade-cont">{{userInfo.Grade}}</text>
-		</view>
-		<view class="user-activity">
-			<view >
-				<view class="cont" @click="toShopservice">
-					<image class="cont-img" src="../../static/image/shopService.png"></image>
-					<text class="cont-title">商家服务</text>
-				</view>
-				<view class="cont" @click="toRecharge">
-					<image class="cont-img" src="../../static/image/withdrawal.png"></image>
-					<text class="cont-title">提现申请</text>
-				</view>
-				<view class="cont" @click="toRecommend">
-					<image class="cont-img" src="../../static/image/myPartner.png"></image>
-					<text class="cont-title">我的推荐</text>
-				</view>
-				<view class="cont" @click="getWxappQR">
-					<image class="cont-img" src="../../static/image/myQRcode.png"></image>
-					<text class="cont-title">推荐码</text>
-				</view>
+	<view class="main">
+		<view class="top">
+			<image class="background-img" src="../../static/image/background-image.png"></image>
+			
+			<view class="userInfo">			
+				<view class="userInfo-content">
+					<view class="userInfo-left">
+						<view class="user-avatar">
+							<image src="../../static/image/night.png"></image>
+						</view>
+						
+						<view class="user-cont">
+							<view class="user-name">哈哈哈</view>
+							<view class="user-level">消费合伙人</view>
+							<view class="invitation-code">
+								<text>邀请码:12312332</text>
+								<view class="invitation-button">复制</view>
+							</view>
+						</view>
+					</view>
+					
+					<view class="userInfo-right">
+						<view ></view>
+					</view>
+				</view>				
 			</view>
+			
+			<view class="navigation-bar"></view> 
 		</view>
 		
-		<view class="wrap">
-			<u-row gutter="16">
-				<u-col span="4">
-					<view class="demo-layout bg-color0" @click="toUserAccout('Bonus')">
-						<view class="cont">
-							<view class="account-name" >津贴权益</view>
-							<view class="account-amount">{{AccoutAmount.BonusAmount}}</view>
-						</view>
-					</view>
-				</u-col>
-				<u-col span="4">
-					<view class="demo-layout bg-color1" @click="toUserAccout('Integral')">
-						<view class="cont">
-							<view class="account-name">补贴账户</view>
-							<view class="account-amount">{{AccoutAmount.IntegralAmount}}</view>
-						</view>
-					</view>
-				</u-col>
-				<u-col span="4">
-					<view class="demo-layout bg-color2" @click="toUserAccout('Recharge')">
-						<view class="cont">
-							<view class="account-name">消费账户</view>
-							<view class="account-amount">{{AccoutAmount.RechargeAmount}}</view>
-						</view>
-					</view>
-				</u-col>
-			</u-row>
-		</view>
-		<view class="wrap">		
-			<u-row gutter="16" >
-				<u-col span="4">
-					<view class="demo-layout bg-color3" @click="toUserAccout('Profit')">
-						<view class="cont">
-							<view class="account-name">余额</view>
-							<view class="account-amount">{{AccoutAmount.ProfitAmount}}</view>
-						</view>	
-					</view>
-				</u-col>
-				<u-col span="4">
-					<view class="demo-layout bg-color4" @click="toUserAccout('Credit')">
-						<view class="cont">
-							<view class="account-name">商家账户</view>
-							<view class="account-amount">{{AccoutAmount.CreditAmount}}</view>
-						</view>	
-					</view>
-				</u-col>
-				<u-col span="4"></u-col>	
-			</u-row>
-		</view>
+			
 		
-		<view class="bottom">		
-			
-			<u-row gutter="12">
-				<u-col span="2">
-					<view class="demo-layout1 ">
-						<image src="../../static/image/lotter.png"></image>
-					</view>
-				</u-col>
-				<u-col span="8">
-					<view class="demo-layout ">
-						我的奖券
-					</view>
-					<u-line color="#e4e7ed" />
-				</u-col>
-				
-				<u-col span="2">
-					<view class="demo-layout ">
-						<u-icon name="arrow-right" size="36"></u-icon>
-					</view>
-				</u-col>
-			</u-row>
-			
-			<u-row gutter="12">
-				<u-col span="2">
-					<view class="demo-layout1 ">
-						<image src="../../static/image/bill.png"></image>
-					</view>
-				</u-col>
-				<u-col span="8">
-					<view class="demo-layout ">
-						我的账单
-					</view>
-					<u-line color="#e4e7ed" />
-				</u-col>
-				
-				<u-col span="2">
-					<view class="demo-layout ">
-						<u-icon name="arrow-right" size="36"></u-icon>
-					</view>
-				</u-col>
-			</u-row>
-			
-			<u-row gutter="12">
-				<u-col span="2">
-					<view class="demo-layout1 ">
-						<image src="../../static/image/project.png"></image>
-					</view>
-				</u-col>
-				<u-col span="8">
-					<view class="demo-layout ">
-						我的项目
-					</view>
-					<u-line color="#e4e7ed" />
-				</u-col>
-				
-				<u-col span="2">
-					<view class="demo-layout ">
-						<u-icon name="arrow-right" size="36"></u-icon>
-					</view>
-				</u-col>
-			</u-row>
-			
-			<u-row gutter="12" >
-				<u-col span="2">
-					<view class="demo-layout1 ">
-						<image src="../../static/image/setting.png"></image>
-					</view>
-				</u-col>
-				<u-col span="8" @click="toSetting">
-					<view class="demo-layout ">
-						设置
-					</view>
-					<u-line color="#e4e7ed" />
-				</u-col>
-				
-				<u-col span="2">
-					<view class="demo-layout ">
-						<u-icon name="arrow-right" size="36"></u-icon>
-					</view>
-				</u-col>
-			</u-row>
-		</view>
 		
 		<!-- 反馈组件 -->
 		<u-toast ref="uToast" />
@@ -174,7 +43,7 @@
 </template>
 
 <script>
-import {request,userRequest} from "@/api/request.js"	
+import {request} from "@/api/request.js"	
 	export default {
 		data() {
 			return {
@@ -309,159 +178,120 @@ import {request,userRequest} from "@/api/request.js"
 	}
 </script>
 
-<style lang="scss" scoped>
-	.back-img{
-		width: 100%;
-		height: 450rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		.cont-img{
-			width: 100%;
-			height: 450rpx;
-		}
-	}
-	
-	.avatar{
-		
-		width: 100%;
-		height: 200rpx;
+<style lang="less" scoped>
+page{
+	width: 100%;
+	height: 100%;
+	background-color: #f5f5f5;
+}	
+.main{
+	.top{
+		width: 100%;	
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		position: absolute;
-		top: 160rpx;
-		
-		.grade-img{
-			width: 240rpx;
-			height: 50x;
+		.background-img{
+			width: 100%;
+			height: 400rpx;
+		}
+		.userInfo{
+			width: 100%;
+			height: 150rpx;
 			position: relative;
-			top: -20rpx;
-		}
-		.grade-cont{
-			position: absolute;
-			top: 136rpx;
-			color: #F1F1F1;
-		}
-		
-	}
-	
-	.user-activity{
-		width: 100%;
-		height: 180rpx;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin-top: -15rpx;
-		
-		view{
-			width: 710rpx;
-			height: 185rpx;
+			top: -270rpx;
 			display: flex;
-			justify-content: space-around;
+			justify-content: center;
 			align-items: center;
 			
-			.cont{
-				width: 150rpx;
-				height: 160rpx;
+			.userInfo-content{
+				width: 710rpx;
+				height: 150rpx;
 				display: flex;
-				flex-direction: column;
+				justify-content: center;
 				align-items: center;
-				.cont-img{
-					width: 85rpx;
-					height: 85rpx;
-				}
-				.cont-title{
-					margin-top: -20rpx;
-				}
-			}
-		}
-	}
-
-	.wrap {
-			padding: 15rpx;
-		}
-	
-		.u-row {
-			margin: 40rpx 0;
-			background-color: #FFFFFF;
-		}
-	
-		.demo-layout {
-			height: 110rpx;
-			border-radius: 8rpx;
-			display: flex;
-			align-items: center;
-			font-size: 30rpx;
-			
-			.cont{
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				position: relative;
-				left: 20rpx;
-				top: 5rpx;
-				.account-name{
-					width: 180rpx;
-					height: 50rpx;
-					color: #FFFFFF;
-					font-size: 28rpx;
-					font-weight: bold;
-					margin-bottom: -10rpx;
+				
+				.userInfo-left{
+					width: 470rpx;
+					height: 150rpx;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					
+					.user-avatar{
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						image{
+							width: 120rpx;
+							height: 120rpx;
+						}
+					}
+					.user-cont{
+						width: 350rpx;
+						height: 150rpx;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-around;
+						
+						.user-name{
+							font-size: 36rpx;
+							color: rgba(0, 0, 0, 1);
+							font-family:PingFang SC;
+							font-weight:600;
+						}
+						.user-level{
+							font-size:24rpx;
+							font-family:PingFang SC;
+							font-weight:400;
+							color:rgba(77,77,77,1);
+						}
+						.invitation-code{
+							width: 350rpx;
+							height: 35rpx;
+							display: flex;
+							justify-content: flex-start;
+							align-items: center;
+							text{
+								width: 190rpx;
+								font-size:24rpx;
+								font-family:PingFang SC;
+								font-weight:400;
+								color:rgba(77,77,77,1);
+							}
+							.invitation-button{
+								width:70rpx;
+								height:30rpx;
+								border:2rpx solid rgba(124,118,81,1);
+								border-radius:10rpx;
+								font-size:22rpx;
+								display: flex;
+								justify-content: center;
+								align-items: center;
+								position: relative;
+								left: 30rpx;
+							}
+						}
+					}
 				}
 				
-				.account-amount{
-					width: 180rpx;
-					height: 50rpx;
-					color: #FFFFFF;
-					font-size: 28rpx;
-					font-weight: bold;
+				.userInfo-right{
+					width: 240rpx;
+					height: 150rpx;
+					background-color: #4CD964;
 				}
 			}
+			
 		}
-	
-		.bg-color0{
-			background-image: linear-gradient(to right, #6CACFF,#8DEBFF );
-		}
-		.bg-color1{
-			background-image: linear-gradient(to right, #A16BFE,#DEB0DF );
-		}
-		.bg-color3{
-			background-image: linear-gradient(to right, #f35670,#ff80a5 );
-		}
-		.bg-color2{
-			background-image: linear-gradient(to right, #ff4a46,#ff7562 );
-		}
-		.bg-color4{
-			background-image: linear-gradient(to right, #5583EE,#41D8DD );
-		}
-		
-	.bottom{
-		padding: 15rpx;
-	}	
-	.demo-layout1{
-		width: 110rpx;
-		height: 110rpx;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		image{
-			width: 50rpx;
+		.navigation-bar{
+			width: 100%;
 			height: 50rpx;
+			position: relative;
+			top: -215rpx;
 		}
 	}
-	.bg-purple {
-		background: #d3dce6;
-	}
+}	
 
-	.bg-purple-light {
-		background: #e5e9f2;
-	}
 	.wxQRimg{
-		
-		// display: flex;
-		// justify-content: center;
-		// align-items: center;
 		.qr-img{
 			width: 450rpx;
 			height: 450rpx;
