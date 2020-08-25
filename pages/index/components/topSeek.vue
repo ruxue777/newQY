659d1ec417cb926eca53f372f6a56cead9c90a7d
Ruxue <1078134231@qq.com>
2020-08-25 20:33:58 +0800

    完成临时二维码的弹窗展示

--- pages/index/components/topSeek.vue
+++ pages/index/components/topSeek.vue
@@ -24,48 +24,225 @@
 				</view>
 			</u-col>
 		</u-row>			 
-	</view>
 	
+	<!-- 反馈组件 -->
+	<u-toast ref="uToast" />
+	<u-popup v-model="show" mode="bottom" border-radius="15" closeable="true" close-icon-pos="top-left">
+		<view class="popup-content">
+			<view class="top">
+				<text class="top-title">付款详情</text>
+				<text class="pay-amount">实付:￥<text class="btn">{{popup_Amount}}</text></text>
+			</view>
+			
+			<view class="middle">
+				<view class="subsidy">
+					<view class="left-cont">
+						<image src="../../../static/image/coupon.png"></image>
+						<text>{{DeductionTips}}</text>
+						<view class="title">补贴抵扣</view>
+					</view>
+					<view class="right-cont">
+						<u-switch v-model="deductionSwitch" size='40' active-color="#fa3534" :disabled="isDeduction"></u-switch>
+					</view>
+				</view>
+				
+				<view class="payment-method">
+					<view class="payment-method-title">选择付款方式</view>
+					<view class="payment-method-content">
+						
+						<view class="wx-account Selected" v-if="payment_Method === 2">
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_wxred.png"></image>
+								<text>微信支付</text>
+							</view>
+							<view class="right">
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>		
+						</view>
+						<view class="wx-account" v-else @click="select_Payment(2)">
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_wx.png"></image>
+								<text>微信支付</text>
+							</view>
+							<view class="right">
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>		
+						</view>
+						
+						<view class="jt-account Selected" v-if="payment_Method === 0" >
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_moneyred.png"></image>
+								<text>津贴账户</text>
+							</view>
+							<view class="right">
+								<view class="account-amount">{{AccoutAmount.ProfitAmount}}</view>
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>
+						</view>
+						<view class="jt-account" v-else @click="select_Payment(0)">
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_money.png"></image>
+								<text>津贴账户</text>
+							</view>
+							<view class="right">
+								<view class="account-amount">{{AccoutAmount.ProfitAmount}}</view>
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>
+						</view>
+											
+						<view class="bt-account Selected" v-if="payment_Method === 1">
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_homered.png"></image>
+								<text>补贴账户</text>
+							</view>
+							<view class="right">
+								<view class="account-amount">{{AccoutAmount.RechargeAmount}}</view>
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>								
+						</view>
+						<view class="bt-account" v-else @click="select_Payment(1)">
+							<view class="left">
+								<image class="left-img-0" src="../../../static/image/pop_home.png"></image>
+								<text>补贴账户</text>
+							</view>
+							<view class="right">
+								<view class="account-amount">{{AccoutAmount.RechargeAmount}}</view>
+								<u-icon class="btn" name="checkmark-circle-fill" size="38"></u-icon>
+							</view>								
+						</view>
+						
+					</view>
+				</view>
+			</view>
+			
+			<view class="bottom">
+				<view class="button" @click="Pay()">
+					立即支付
+				</view>
+			</view>
+		</view>
+	</u-popup>
+	<u-popup v-model="Pay_popup" mode="center" border-radius="15" closeable="true" close-icon-pos="top-left">
+		<view class="cont">
+			<text class="title">请输入支付密码</text>
+			<input type="password" v-model="PayPwd"/>
+			<text class="mini">忘记支付密码</text>
+			<u-button shape="circle" size="medium" ripple="true"  type="warning" @click="virtual_Payment()">确认支付</u-button>
+		</view>
+	</u-popup>
+	</view>
 </template>
 
 <script>
+import {request} from '@/api/request.js'	
 	export default {
 		props:['LocationData'],
 		data() {
 			return {
-				keyword:''
+				//支付弹窗
+				show:false,
+				//支付方式
+				payment_Method:4,
+				//账户金额
+				AccoutAmount:[],
+				//订单详情
+				orderDetails:[],
+				//抵扣开关
+				deductionSwitch:true,
+				//抵扣禁用开关
+				isDeduction:false,
+				//支付金额
+				popup_Amount:'',
+				//抵扣提示
+				DeductionTips:''
 			}
 		},
+		watch:{
+			deductionSwitch(val){
+				if(val == false){
+					this.DeductionTips = '不使用或不支持抵扣'
+					
+				 	this.popup_Amount =  this.orderDetails.CR_AppAmount
+				}else{
+					
+					this.DeductionTips = `补贴账户已抵扣￥${res.CR_OffsetAmount}`;
+					
+					this.popup_Amount = (res.CR_AppAmount - res.CR_OffsetAmount).toFixed(2)
+				}
+			}	
+		},
 		methods: {
+			//扫码方法
 			scanCode(){
-				uni.scanCode({
-					success: (res) => {
-			
-						let moment = res.result.replace(/[^a-zA-Z]/g,'')
+				const userInfo = uni.getStorageSync("globalUser");
+				if(!userInfo){
+					this.$refs.uToast.show({
+						title: `当前未登录哦~`,
+						type: 'warning',
+					})
+					return;
+				}
+				else{
+					this.userInfo = userInfo;
+					this.getAccoutAmount()
+					
+					uni.scanCode({
+						success: (res) => {
+				
+							let Title = res.result.replace(/[^a-zA-Z]/g,'')
+							
+							let Int = res.result.replace(/[^\d]/g,'')
+							
+							if(Title == "FixedCashier"){
+								uni.navigateTo({
+									url:`/pages/pay/scanCodePay?BusinessID=${Int}`
+								})
+							}
+							else if(Title == "TempCashier")
+							{								
+								this.getOrderDetails(Int)
+								this.show = true;
+							}
+							else{
+								uni.showToast({
+									title:'暂不支持识别外部二维码',
+									icon:'none' 
+								})
+								return
+							}
+						 }
+					})
+				}
+			},
+			async getOrderDetails(OrderId){
+				await request('API_AddCashierRecord_Step2_V2',{user_id:this.userInfo.user_id,OrderId:OrderId}).then(res=>{
+					
+					this.orderDetails = res 
+					
+					if(res.CR_OffsetAmount == 0){
+						this.isDeduction = true;
+						this.DeductionTips = '不使用或不支持抵扣';
 						
-						let BusinessID = parseInt(res.result)
+						this.popup_Amount = (res.CR_AppAmount * 1).toFixed(2)
+						this.show = true
+					}else{
+						this.DeductionTips = `补贴账户已抵扣￥${res.CR_OffsetAmount}`;
 						
-						if(moment == "FixedCashier"){
-							uni.navigateTo({
-								url:`/pages/pay/scanCodePay?BusinessID=${BusinessID}`
-							})
-						}
-						else if(moment == "TempCashier")
-						{
-							console.log(res)
-							// uni.navigateTo({
-							// 	url:'/pages/Pay/SYSPay?Temporary=' + encodeURIComponent(JSON.stringify(res))
-							// })
-						}
-						else{
-							uni.showToast({
-								title:'暂不支持识别外部二维码',
-								icon:'none'
-							})
-							return
-						}
-					 }
+						this.popup_Amount = (res.CR_AppAmount - res.CR_OffsetAmount).toFixed(2)
+						this.show = true
+					}		
+					
 				})
+			},
+			//获得账户金额
+			getAccoutAmount(){
+				request('API_GetAccountAmount',{user_id:this.userInfo.user_id}).then(res=>{
+					this.AccoutAmount = res
+				})
+			},
+			//选择支付方式
+			select_Payment(e){
+				this.payment_Method = e
 			},
 			to_Search(){
 				console.log('去搜索页面')
@@ -152,5 +329,238 @@
 	font-weight: bold;
 	margin-left: 12rpx;
 	margin-top: -5rpx;
+}
+.popup-content{
+	width: 100%;
+	height: 1000rpx;
+	background-color: #fff;
+	
+	.top{
+		width: 100%;
+		height: 210rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+		
+		.top-title{
+			font-size:36rpx;
+			font-family:PingFang SC;
+			font-weight:500;
+			color:rgba(0,0,0,1);
+		}
+		
+		.pay-amount{
+			font-size:30rpx;
+			font-family:PingFang SC;
+			font-weight:500;
+			color:rgba(0,0,0,1);
+			
+			.btn{
+				font-size:50rpx;
+				font-family:PingFang SC;
+				font-weight:500;
+				color:rgba(0,0,0,1);
+			}
+		}
+	}
+	
+	.middle{
+		width: 100%;
+		height: 580rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-radius:20px;
+		-moz-box-shadow:0px 0px 10px #e1e1e1; -webkit-box-shadow:0px 0px 10px #e1e1e1; box-shadow:0px 0px 10px #e1e1e1;
+		
+		.subsidy{
+			width: 670rpx;
+			height: 80rpx;
+			border-bottom: 4rpx solid rgba(250, 250, 250, 1);
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			
+			.left-cont{
+				width: 520rpx;
+				height: 100%;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				
+				image{
+					width: 40rpx;
+					height: 30rpx;
+				}
+				
+				text{
+					font-size:28rpx;
+					font-family:PingFang SC;
+					font-weight:300;
+					color:rgba(255,48,0,1);
+					line-height:46rpx;
+				}
+				
+				.title{
+					width:90rpx;
+					height:25rpx;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					background:rgba(255,104,104,1);
+					border-radius:5rpx 5rpx 5rpx 0rpx;						
+					font-size:20rpx;
+					font-family:Adobe Heiti Std;
+					font-weight:normal;
+					color:rgba(255,255,255,1);
+					line-height:40rpx;
+				}
+			}
+			
+			.right-cont{
+				width: 150rpx;
+				height: 100%;
+				display: flex;
+				justify-content: flex-end;
+				align-items: center;
+			}
+		}
+		
+		.payment-method{
+			width: 670rpx;
+			height: 500rpx;
+			
+			.payment-method-title{
+				width: 100%;
+				height: 80rpx;
+				display: flex;
+				align-items: center;
+				font-size:30rpx;
+				font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+				font-weight:500;
+				color:rgba(51,51,51,1);
+			}
+			
+			.payment-method-content{
+				width: 100%;
+				height: 420rpx;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				align-items: center;
+				image{
+					width: 40rpx;
+					height: 40rpx;
+				}
+				
+				.jt-account,.bt-account,.wx-account{
+					width:670rpx;
+					height:80rpx;
+					border:3rpx solid rgba(225,225,225,1);
+					border-radius:40rpx;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					
+					.left{
+						width: 215rpx;
+						height: 40rpx;
+						display: flex;
+						justify-content: space-around;
+						align-items: center;
+						border-right: 2rpx solid #efefef;
+					}
+					
+					.right{
+						width: 360rpx;
+						height: 50rpx;
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+						position: relative;
+						
+						.account-amount{
+							position: absolute;
+							left: 20rpx;
+							font-size:26rpx;
+							font-family:PingFang SC;
+							font-weight:400;
+							color:rgba(102,102,102,1);
+							line-height:40rpx;
+						}
+						
+						.btn{
+							position: absolute;
+							right: 0;
+						}
+					}
+				}
+		
+				.Selected{
+					border:3rpx solid rgba(255, 48, 0, 1);
+					color:rgba(255, 48, 0, 1);
+				}
+			}
+		}
+	}
+	
+	.bottom{
+		width: 100%;
+		height: 200rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+		.button{
+			width: 670rpx;
+			height: 80rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background:rgba(255,229,14,1);
+			border-radius:40rpx;
+			font-size:36rpx;
+			font-family:PingFang SC;
+			font-weight:600;
+			color:rgba(51,51,51,1);
+		}
+	}
+}
+.cont{
+	width: 500rpx;
+	height: 500rpx;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	
+	.title{
+		width: 480rpx;
+		height: 120rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 32rpx;
+		font-weight: bold;
+	}
+	input{
+		width: 380rpx;
+		height: 80rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1rpx solid #999999;
+		font-size: 30rpx;
+	}
+	.mini{
+		width: 480rpx;
+		height: 120rpx;
+		font-size: 24rpx;
+		color: red;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 }
 </style>
